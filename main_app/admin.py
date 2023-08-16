740b87631e66af96d0bd7934ef41dfc8e7c6f5a6
pxner0 <napogonowski@outlook.com>
2023-08-16 11:14:46 +1000

    admin access extended, detail page cleaned up

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Plant, Feeding
+from .models import Plant, Feeding, Extra
 # Register your models here.
 admin.site.register(Plant)
 admin.site.register(Feeding)
+admin.site.register(Extra)
